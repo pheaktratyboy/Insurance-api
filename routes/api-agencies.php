41323b78cc78c61eb5ba6f86c43cbea880af206e
pheaktra_leo <pheaktraty@gmail.com>
2022-04-20 08:30:40 +0700

    remove route agencies/all

--- routes/api-agencies.php
+++ routes/api-agencies.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\AgencyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('agencies', [AgencyController::class,'index'])->name('agencies.index');
-Route::get('agencies/all', [AgencyController::class,'getAllAgency'])->name('agencies.getAllAgency');
 Route::get('agencies/{agency}', [AgencyController::class,'show'])->name('agencies.show');
 
 Route::post('agencies', [AgencyController::class,'store'])->name('agencies.store');
