548dae03664578662c4168caf87f8508740f8b1c
pheaktra_leo <pheaktraty@gmail.com>
2022-05-04 16:37:08 +0700

    add new route

--- routes/api-employees.php
+++ routes/api-employees.php
@@ -7,5 +7,5 @@ Route::get('employees', [EmployeeController::class,'index'])->name('employees.in
 Route::get('employees/getAllStaff', [EmployeeController::class,'getAllStaff'])->name('employees.getAllStaff');
 Route::get('employees/{user}', [EmployeeController::class,'show'])->name('employees.show');
 
-
+Route::post('employees/store-by-user', [EmployeeController::class,'store'])->name('employees.store_by_user');
 Route::put('employees/{employee}', [EmployeeController::class,'update'])->name('employees.update');
