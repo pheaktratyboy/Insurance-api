b6c27ad059fc235dbd7b2afde145914fccad42f1
pheaktra_leo <pheaktraty@gmail.com>
2022-05-12 14:13:09 +0700

    refactor route

--- routes/api-reports.php
+++ routes/api-reports.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
 
-Route::get('reports/reportYearly', [ReportController::class,'reportYearly'])->name('reports.report_yearly');
-Route::get('reports/reportDashboard', [ReportController::class,'reportDashboard'])->name('reports.report_dashboard');
-Route::get('reports/reportSubscriber', [ReportController::class,'reportSubscriber'])->name('reports.report_subscriber');
+Route::get('reports/yearly', [ReportController::class,'reportYearly'])->name('reports.report_yearly');
+Route::get('reports/dashboard', [ReportController::class,'reportDashboard'])->name('reports.report_dashboard');
+Route::get('reports/subscriber', [ReportController::class,'reportSubscriber'])->name('reports.report_subscriber');
